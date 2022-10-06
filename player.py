5719ba9b1b93b705fc1e3362a10d2c88acc2dc55
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-10-06 17:42:00 +0900

    add player class

--- player.py
+++ player.py
@@ -1,8 +1,17 @@
 from yut import yut
+from token import token
 
 
 class player:
-    yut_list = [yut(), yut(), yut(), yut()]  # 나중에 윷리스트는 게임으로 옮김
+    team = ""
+    tokens = []
+
+    def __init__(self, team):
+        self.team = team
+        self.tokens = [token(), token(), token(), token()]
+
+    # 나중에 윷리스트는 게임으로 옮기고 yut_list를 throw 함수의 인자로 받음
+    yut_list = [yut(), yut(), yut(), yut()]
 
     def throw(self):
         li = [i.throw() for i in self.yut_list]
@@ -19,3 +28,13 @@ class player:
             return "윷"
         else:
             return "error"
+
+    def get_team(self):
+        return self.team
+
+    def goal_in_token(self):
+        count = 0
+        for i in self.tokens:
+            if i.is_goal_in:
+                count += 1
+        return count
