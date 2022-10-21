0e9ccdf20e3058032b20572b1c059f01fae6d3f1
cjsqudwns <59379460+cjsqudwns@users.noreply.github.com>
2022-10-21 23:21:32 +0900

    game title change
    game

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 from yut import yut
 from board import board
+from time import sleep
+import keyboard
+from gotoxy import gotoxy
+import os
 
 
 class game:
@@ -26,5 +30,54 @@ class game:
         else:
             self.turn = 1
 
-    def game_title(self):
-        return 0
+
+def game_title(self):
+    cursor_x = 20
+    cursor_y = 5
+    x = 10
+    y = 1
+    os.system("cls")
+    while True:
+
+        gotoxy(x, y + 0)
+        print("TRADITIONAL-STICK-PLAY")
+        gotoxy(x, y + 4)
+        print("START")
+        gotoxy(x, y + 6)
+        print("QUIT")
+        gotoxy(x, y + 8)
+        print("HELP")
+
+        gotoxy(cursor_x, cursor_y)
+        print("◀")
+
+        if keyboard.is_pressed(80) and cursor_y < 9:  # 아래쪽 방향키 입력
+            sleep(0.2)
+            cursor_y += 2
+            os.system("cls")
+        if keyboard.is_pressed(72) and cursor_y > 5:  # 위쪽 방향키 입력
+            sleep(0.2)
+            cursor_y -= 2
+            os.system("cls")
+
+        if keyboard.is_pressed("enter") and cursor_y == 5:  # START
+            os.system("cls")
+            gotoxy(x, y + 2)
+            print("TRADITIONAL-STICK-PLAY")
+            gotoxy(x + 6, y + 5)
+            print("시작합니다")
+            sleep(1)
+            return 0  # 리턴 값으로 다른 동작 수행?
+
+        if keyboard.is_pressed("enter") and cursor_y == 7:  # QUIT
+            os.system("cls")
+            gotoxy(x, y + 2)
+            print("TRADITIONAL-STICK-PLAY")
+            gotoxy(x + 6, y + 5)
+            print("종료합니다")
+            sleep(1)
+            return 0
+
+        if keyboard.is_pressed("enter") and cursor_y == 9:  # HELP
+            print("help")
+            return 0
