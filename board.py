ff586ee2acab4ac8a237c34ab41141467fd721ee
leesung2925 <59409742+leesung2925@users.noreply.github.com>
2022-10-22 17:22:42 +0900

    c:; cd 'c:\Users\이성민\전기프\Traditional-Stick-Play'; & 'C:\Users\김성민\AppData\Local\Programs\Python\Python310\python.exe' 'c:\Users\김성민\.vscode\extensions\ms-python.python-2022.16.0\pythonFiles\lib\python\debugpy\adapter/../..\debugpy\launcher' '3942' '--' 'c:\Users\이성민\전기프\Traditional-Stick-Play\main.py'

--- board.py
+++ board.py
@@ -4,6 +4,10 @@ from piece import piece
 from gotoxy import gotoxy
 import os
 
+GOALIN = 0
+MOVE = 1
+CATCH = 2
+
 
 class board:
     tile_list = []  # 칸의 배열
@@ -12,13 +16,29 @@ class board:
         for i in range(29):
             self.tile_list.append(tile(i + 1))
 
-    def move_token(self, token, yut_result):
+    def move_token(self, token, yut_result) -> int:
+        i = -1
         if token.get_index() == "골인":
-            return 0
+            return GOALIN
         else:
-            result = self.tile_list[token.get_index()].get_dest_index(yut_result)
-            token.set_index(result)
-            self.tile_list[result].reach_token(token)
+            i = MOVE
+            result = self.tile_list[token.get_index()].get_dest_index(
+                yut_result
+            )  # result변수에 윷 결과에 맞는 도착할 칸의 인덱스 저장
+            if len(self.tile_list[result].get_tokens()) > 0:
+                if (
+                    self.tile_list[result].get_tokens()[0].get_team()
+                    != token.get_team()
+                ):  # result칸에 있는 타일의 말 리스트를 가져와 리스트에 적팀 말이 있는지 조사
+                    for i in self.tile_list[result].get_tokens():
+                        i.set_index(0)  # 있으면 적팀 말 집으로 다 보내버리고 해당 칸의 말 리스트 clear()
+                    self.tile_list[result].get_tokens().clear()
+                    i = CATCH  # i값을 catch로 설정
+            for i in self.tile_list[token.get_index()]:
+                self.tile_list[result].reach_token(token)
+                token.set_index(result)
+
+        return i
 
     def print_tile(self, idx):
         num_of_tokens = len(self.tile_list[idx - 1].get_tokens())
@@ -74,14 +94,17 @@ class board:
     def show_tokens_state(self, player1: player, player2: player):
         player1_token_list = player1.get_tokenlist()
         player2_token_list = player2.get_tokenlist()
-        gotoxy(27, 0)
+        gotoxy(28, 0)
         print(player1.get_team() + " 팀")
         for i in range(len(player1_token_list)):  # 각 플레이어의 말 상태 출력
-            gotoxy(27, i + 1)
-            print("%d 번 말 : %d" % (i, player1_token_list[i].get_index()))
-        gotoxy(38, 0)
+            gotoxy(28, i + 1)
+            print("%d 번 말 : %d" % (i + 1, player1_token_list[i].get_index()))
+        gotoxy(39, 0)
         print("| " + player2.get_team() + " 팀")
         for i in range(len(player2_token_list)):
-            gotoxy(38, i + 1)
-            print("| %d 번 말 : %d" % (i, player2_token_list[i].get_index()))
-        gotoxy(0, 15)
+            gotoxy(39, i + 1)
+            print("| %d 번 말 : %d" % (i + 1, player2_token_list[i].get_index()))
+
+    def show_yut_result(self, player: player):
+        gotoxy(28, 7)
+        print("윷 결과")
