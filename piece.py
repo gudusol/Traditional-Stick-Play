bfc521bbf2ba1045afa2fe8164f80b8acfdeb121
taeseung-yoo <59465914+taeseung-yoo@users.noreply.github.com>
2022-10-23 08:19:38 +0900

    필요없는 부분 삭제

--- piece.py
+++ piece.py
@@ -10,14 +10,6 @@ class piece:
         self.idx = idx
 
     def get_index(self):
-        # if self.idx == 0:
-        #    return 0
-        # elif self.idx > 0 and self.idx < 30:
-        #    return self.idx
-        # elif self.idx >= 30:
-        #    return self.idx
-        # else:
-        #    return self.idx  # idx 음수값
         return self.idx
 
     def get_team(self):
--- player.py
+++ player.py
@@ -33,14 +33,6 @@ class player:
         else:
             return "error"
 
-    def move_piece(self, piece, yut_result):
-        if piece.get_index() == "골인":  # 움직이려는 말이 이미 골인한 말일 때
-            return -1
-        else:
-            result = self.tile_list[piece.get_index()].get_dest_index(yut_result)
-            piece.set_index(result)
-            self.tile_list[result].reach_piece(piece)
-
     def get_team(self):
         return self.team
 
