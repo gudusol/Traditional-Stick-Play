5702a3162d1a590567e1fe5c387bafad0c725289
leesung2925 <59409742+leesung2925@users.noreply.github.com>
2022-10-22 22:22:52 +0900

    move_piece 수정

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from game import game
+from board import board
+from player import player
+import os
 
-
-g = game()
-g.game_start()
+b = board()
+p1 = player("red")
+p2 = player("blue")
+b.move_piece(p1.get_piecelist()[0], 3)
+b.move_piece(p1.get_piecelist()[0], 2)
+os.system("cls")
+b.show_board()
+b.show_pieces_state(p1, p2)
